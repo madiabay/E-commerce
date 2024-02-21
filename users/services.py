faed943e3017fa05547d9fc47d08dc31a279fd42
Madi <Madi.Abay@kcell.kz>
2024-02-21 10:47:33 +0600

    verify tokens and refactor my code

--- users/services.py
+++ users/services.py
@@ -18,20 +18,14 @@ class UserServicesInterface(Protocol):
 
     def create_token(self, data: OrderedDict) -> dict: ...
 
+    def verify_token(self, data: OrderedDict) -> dict: ...
+
 
 class UserServicesV1:
     user_repos: repos.UserReposInterface = repos.UserReposV1()
 
     def create_user(self, data: OrderedDict) -> dict:
-        numbers = [str(i) for i in range(10)]
-        code = ''.join(random.choices(numbers, k=4))
-        session_id = str(uuid.uuid4())
-        session = {'code': code, **data}
-        cache.set(session_id, session, timeout=300)
-        self._send_sms_to_phone_number(phone_number=data['phone_number'], code=code)
-        self._mobizon(phone_number=data['phone_number'][1:], code=code)
-
-        return {'session_id': session_id}
+        return self._verify_phone_number(data=data)
 
     def verify_user(self, data: OrderedDict) -> None:
         user_data = cache.get(data['session_id'])
@@ -47,6 +41,44 @@ class UserServicesV1:
         })
         self._send_letter_to_email(email=user.email)
 
+    def create_token(self, data: OrderedDict) -> dict:
+        user = self.user_repos.get_user(data=data) # проверка на наличие user
+        return self._verify_phone_number(data=data)
+
+    def verify_token(self, data: OrderedDict) -> dict:
+        session = cache.get(data['session_id'])
+        if not session:
+            return
+
+        if session['code'] != data['code']:
+            return
+
+        user = self.user_repos.get_user(data={'phone_number': session['phone_number']})
+        access = tokens.AccessToken.for_user(user=user)
+        refresh = tokens.RefreshToken.for_user(user=user)
+
+        return {
+            'access': str(access),
+            'refresh': str(refresh),
+        }
+
+    def _verify_phone_number(self, data: OrderedDict) -> dict:
+        code = self._generate_code()
+        session_id = self._generate_session_id()
+        cache.set(session_id, {**data, 'code': code}, timeout=300)
+        self._send_sms_to_phone_number(phone_number=data['phone_number'], code=code)
+        # self._mobizon(phone_number=data['phone_number'][1:], code=code)
+        return {'session_id': session_id}
+
+    @staticmethod
+    def _generate_code(length: int = 4) -> str:
+        numbers = [str(i) for i in range(10)]
+        return ''.join(random.choices(numbers, k=length))
+
+    @staticmethod
+    def _generate_session_id() -> str:
+        return str(uuid.uuid4())
+
     @staticmethod
     def _send_letter_to_email(email: str) -> None:
         print(f'send letter to {email}')
@@ -60,7 +92,8 @@ class UserServicesV1:
         url = 'https://api.mobizon.kz'
         api_key = 'kza3095687d4a6080881fe37a80ba33cff4e5581c9d82db6e90b5095e84f1ef2628174'
         client = MobizonClient(url=url, api_key=api_key)
-        result = client.send_message(recipient=phone_number, text=f'send sms code {code} from MADI', sender_signature=None)
+        result = client.send_message(recipient=phone_number, text=f'send sms code {code} from MADI',
+                                     sender_signature=None)
         sleep(3)
         result = client.get_message_status([result.message_id])
         print()
@@ -69,13 +102,3 @@ class UserServicesV1:
         # assert result[0].status == 'DELIVRD'
         client.close()
 
-    def create_token(self, data: OrderedDict) -> dict:
-        user = self.user_repos.get_user(data=data)
-
-        access = tokens.AccessToken.for_user(user=user)
-        refresh = tokens.RefreshToken.for_user(user=user)
-
-        return {
-            'access': str(access),
-            'refresh': str(refresh),
-        }
--- users/urls/v1.py
+++ users/urls/v1.py
@@ -2,16 +2,14 @@ from django.urls import path
 from users import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
+    TokenRefreshView
 )
 
 
 urlpatterns = [
     path('users/create/', views.UserViewSet.as_view({'post': 'create_user'})),
     path('users/verify/', views.UserViewSet.as_view({'post': 'verify_user'})),
-    # path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('users/token/', views.UserViewSet.as_view({'post': 'create_token'}), name='token_obtain_pair'),
+    path('users/token/', views.UserViewSet.as_view({'post': 'create_token'})),
+    path('users/token/verify/', views.UserViewSet.as_view({'post': 'verify_token'})),
     path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('users/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
